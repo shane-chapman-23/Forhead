d518ab21efec0c106326713d18b905fa1e15df4d
shane-chapman-23 <chapmanshane23@gmail.com>
2025-01-10 22:08:35 +1300

    added tilemap and auto tilemap drawer

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,8 +4,24 @@ public class Player : MonoBehaviour
 {
     public static Player Instance;
 
+    private Rigidbody2D _RB;
+
+    public float moveSpeed;
+
     private void Awake()
     {
         Instance = this;
+
+        _RB = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        _RB.AddForce(Vector3.right * moveSpeed * Head.Instance.PlayerMoveDirection, ForceMode2D.Impulse);
     }
 }
